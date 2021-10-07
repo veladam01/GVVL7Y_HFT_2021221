b6b6b6f284382a7d6ab04a14fe46eda533a0b98f
veladam01 <veladam01@gmail.com>
2021-10-07 09:31:26 +0200

    default created Class1 files removed

--- GVVL7Y_HFT_2021221.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GVVL7Y_HFT_2021221.Models
-{
-    public class Class1
-    {
-    }
-}
--- GVVL7Y_HFT_2021221.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GVVL7Y_HFT_2021221.Test
-{
-    public class Class1
-    {
-    }
-}
