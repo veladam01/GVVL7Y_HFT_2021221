72a3385ad7c2dcb80b4aff1d15c6f57b5a1fb81d
veladam01 <veladam01@gmail.com>
2021-10-13 20:44:51 +0200

    GitDatabaseContext.cs OnModelCreating method updated with FluentAPI and HasData

--- GVVL7Y_HFT_2021221.Data/GitDatabaseContext.cs
+++ GVVL7Y_HFT_2021221.Data/GitDatabaseContext.cs
@@ -39,79 +39,98 @@ namespace GVVL7Y_HFT_2021221.Data
             GitRepo dayofdeath = new GitRepo() { ID = 3, Name = "Day Of Death Source Code", OwnerID = krakenattack.ID, Created = Convert.ToDateTime("2020-04-20") };
             GitRepo riseofundead = new GitRepo() { ID = 2, Name = "Rise of Undead Source Code", OwnerID = krakenattack.ID, Created = Convert.ToDateTime("2019-06-09") };
 
-            GitCommit commit1 = new GitCommit()
+            //FluentAPI, not sure if needed
+            modelBuilder.Entity<GitCommit>(entity =>
             {
-                //ID = Convert.ToInt32("9e8f9dab1", 16),
-                ID =1,
-                CommiterID = veladam01.ID,
-                CommitMessage = "Add .gitignore and .gitattributes.",
-                TargetRepositoryID = GVVL7Y_HFT_2021221.ID,
-                When = Convert.ToDateTime("2021. 10. 06. 22:18:24")
-            };
-            GitCommit commit2 = new GitCommit()
-            {
-                //ID = Convert.ToInt32("07415080", 16),
-                ID = 2,
-                CommiterID = veladam01.ID,
-                CommitMessage = "Add project files",
-                TargetRepositoryID = GVVL7Y_HFT_2021221.ID,
+                entity.HasOne(comitter => comitter.User)
+                .WithMany(users => users.Commits)
+                .HasForeignKey(committer => committer.CommiterID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+                entity.HasOne(target => target.Repo)
+                .WithMany(repos => repos.Commits)
+                .HasForeignKey(target => target.TargetRepositoryID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
                 
-                When = Convert.ToDateTime("2021. 10. 06. 22:18:26")
-            };
+            });
 
-            GitCommit roucommit1 = new GitCommit()
+            /*modelBuilder.Entity<GitUser>(entity =>
             {
-                //ID = Convert.ToInt32("07415080", 16),
-                ID = 3,
-                CommiterID = krakenattack.ID,
-                CommitMessage = "Project startup commit",
-                TargetRepositoryID = riseofundead.ID,
+                entity.HasMany(repos => repos.Repos)
+                  .WithOne(owner => owner.Owner)
+                  .HasForeignKey(owner => owner.ID)
+                  .OnDelete(DeleteBehavior.ClientSetNull);
+            }
+            );*/
 
-                When = Convert.ToDateTime("2019. 06. 09.")
-            };
-            GitCommit roucommit2 = new GitCommit()
+            modelBuilder.Entity<GitRepo>(entity =>
             {
-                //ID = Convert.ToInt32("07415080", 16),
-                ID = 4,
-                CommiterID = krakenattack.ID,
-                CommitMessage = "Player entities created",
-                TargetRepositoryID = riseofundead.ID,
+                entity.HasOne(owner => owner.Owner)
+                   .WithMany(repos => repos.Repos)
+                   .HasForeignKey(owner => owner.OwnerID)
+                   .OnDelete(DeleteBehavior.ClientSetNull);
+            }
+            );
+            //FluentAPI end
 
-                When = Convert.ToDateTime("2019. 06. 10.")
-            };
-
-            GitCommit roucommit3 = new GitCommit()
-            {
-                //ID = Convert.ToInt32("07415080", 16),
-                ID = 4,
-                CommiterID = veladam01.ID,
-                CommitMessage = "Weapon models added",
-                TargetRepositoryID = riseofundead.ID,
-
-                When = Convert.ToDateTime("2019. 06. 13.")
-            };
-
-            GitCommit roucommit4 = new GitCommit()
-            {
-                //ID = Convert.ToInt32("07415080", 16),
-                ID = 6,
-                CommiterID = krakenattack.ID,
-                CommitMessage = "Zombie AI added",
-                TargetRepositoryID = riseofundead.ID,
-
-                When = Convert.ToDateTime("2019. 06. 15.")
-            };
-
-            GitCommit dodcommit1 = new GitCommit()
-            {
-                //ID = Convert.ToInt32("07415080", 16),
-                ID = 6,
-                CommiterID = krakenattack.ID,
-                CommitMessage = "Initial commit",
-                TargetRepositoryID = dayofdeath.ID,
-
-                When = Convert.ToDateTime("2020. 04. 20.")
-            };
+            modelBuilder.Entity<GitUser>().HasData(veladam01, krakenattack);
+            modelBuilder.Entity<GitRepo>().HasData(GVVL7Y_HFT_2021221, riseofundead, dayofdeath);
+            modelBuilder.Entity<GitCommit>().HasData(
+                new GitCommit()
+                {
+                    //ID = Convert.ToInt32("9e8f9dab1", 16),
+                    ID = 1,
+                    CommiterID = veladam01.ID,
+                    CommitMessage = "Add .gitignore and .gitattributes.",
+                    TargetRepositoryID = GVVL7Y_HFT_2021221.ID,
+                    When = Convert.ToDateTime("2021. 10. 06. 22:18:24")
+                },
+                new GitCommit()
+                {
+                    //ID = Convert.ToInt32("07415080", 16),
+                    ID = 2,
+                    CommiterID = veladam01.ID,
+                    CommitMessage = "Add project files",
+                    TargetRepositoryID = GVVL7Y_HFT_2021221.ID,
+                    When = Convert.ToDateTime("2021. 10. 06. 22:18:26")
+                },
+                new GitCommit()
+                {
+                    //ID = Convert.ToInt32("07415080", 16),
+                    ID = 3,
+                    CommiterID = krakenattack.ID,
+                    CommitMessage = "Project startup commit",
+                    TargetRepositoryID = riseofundead.ID,
+                    When = Convert.ToDateTime("2019. 06. 09.")
+                },
+                new GitCommit()
+                {
+                    //ID = Convert.ToInt32("07415080", 16),
+                    ID = 4,
+                    CommiterID = veladam01.ID,
+                    CommitMessage = "Weapon models added",
+                    TargetRepositoryID = riseofundead.ID,
+                    When = Convert.ToDateTime("2019. 06. 13.")
+                },
+                new GitCommit()
+                {
+                    //ID = Convert.ToInt32("07415080", 16),
+                    ID = 5,
+                    CommiterID = krakenattack.ID,
+                    CommitMessage = "Zombie AI added",
+                    TargetRepositoryID = riseofundead.ID,
+                    When = Convert.ToDateTime("2019. 06. 15.")
+                },
+                new GitCommit()
+                {
+                    //ID = Convert.ToInt32("07415080", 16),
+                    ID = 6,
+                    CommiterID = krakenattack.ID,
+                    CommitMessage = "Initial commit",
+                    TargetRepositoryID = dayofdeath.ID,
+                    When = Convert.ToDateTime("2020. 04. 20.")
+                }
+                );
         }
     }
 }
