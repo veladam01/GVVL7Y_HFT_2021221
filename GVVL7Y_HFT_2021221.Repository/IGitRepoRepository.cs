9ec67b26b1ded77a4713b9c0fafb163503088cde
veladam01 <veladam01@gmail.com>
2021-10-28 18:22:44 +0200

    IGitRepoRepository interface created

---
+++ GVVL7Y_HFT_2021221.Repository/IGitRepoRepository.cs
@@ -0,0 +1,29 @@
+﻿using GVVL7Y_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Repository
+{
+    public interface IGitRepoRepository
+    {
+        #region CRUD
+        //create
+        void Create(GitRepo gitRepo);
+
+        //readall
+        IQueryable<GitRepo> ReadAll();
+
+        //readone
+        GitRepo ReadOne(int id);
+
+        //update
+        void Update(GitRepo gitRepo);
+
+        //delete
+        void Delete(int id);
+        #endregion
+    }
+}
