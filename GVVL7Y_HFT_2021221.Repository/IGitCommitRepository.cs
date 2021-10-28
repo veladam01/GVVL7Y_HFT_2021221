b2643e166779a8790d5f91bb4bdcf4f203cb4c79
veladam01 <veladam01@gmail.com>
2021-10-28 18:21:09 +0200

    IGitCommitRepository interface created

---
+++ GVVL7Y_HFT_2021221.Repository/IGitCommitRepository.cs
@@ -0,0 +1,29 @@
+﻿using GVVL7Y_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Repository
+{
+    public interface IGitCommitRepository
+    {
+        #region CRUD
+        //create
+        void Create(GitCommit gitCommit);
+
+        //readall
+        IQueryable<GitCommit> ReadAll();
+
+        //readone
+        GitCommit ReadOne(int id);
+
+        //update
+        void Update(GitCommit gitCommit);
+
+        //delete
+        void Delete(int id);
+        #endregion
+    }
+}
