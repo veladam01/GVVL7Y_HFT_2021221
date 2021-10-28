c27157e6abec01dc147d201c1f7ba437616b19ff
veladam01 <veladam01@gmail.com>
2021-10-28 18:16:37 +0200

    IGitUserRepository interface Created

---
+++ GVVL7Y_HFT_2021221.Repository/IGitUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using GVVL7Y_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Repository
+{
+    public interface IGitUserRepository
+    {
+        #region CRUD
+        //create
+        void Create(GitUser gitUser);
+
+        //readall
+        IQueryable<GitUser> ReadAll();
+
+        //readone
+        GitUser ReadOne(int id);
+
+        //update
+        void Update(GitUser gitUser);
+
+        //delete
+        void Delete(int id);
+        #endregion
+    }
+}
