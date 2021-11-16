d2087de95d44d7f56089e1556ccadf974fb23562
veladam01 <veladam01@gmail.com>
2021-11-16 09:56:04 +0100

    GitRepoLogic class created, implements IGitRepoLogic interface

---
+++ GVVL7Y_HFT_2021221.Logic/GitRepoLogic.cs
@@ -0,0 +1,30 @@
+﻿using GVVL7Y_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Logic
+{
+    class GitRepoLogic : IGitRepoLogic
+    {
+        IGitRepoRepository gitRepoRepository;
+
+        public GitRepoLogic(IGitRepoRepository gitRepoRepository)
+        {
+            this.gitRepoRepository = gitRepoRepository;
+        }
+        public int RepoCount()
+        {
+            return gitRepoRepository.ReadAll().Count();
+            //throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> RepoCountByOwners()
+        {
+            return gitRepoRepository.ReadAll().GroupBy(x=>x.Owner).Select(x => new KeyValuePair<string, int>(x.Key.Name, x.Count()));
+            throw new NotImplementedException();
+        }
+    }
+}
