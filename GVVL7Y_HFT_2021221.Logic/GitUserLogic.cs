126469ddd27f5710c3e23cf9c2aa94d5e8cae0cb
veladam01 <veladam01@gmail.com>
2021-11-16 11:21:10 +0100

    GitUserLogic class created, implements IGitUserLogic interface

---
+++ GVVL7Y_HFT_2021221.Logic/GitUserLogic.cs
@@ -0,0 +1,24 @@
+﻿using GVVL7Y_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Logic
+{
+    class GitUserLogic : IGitUserLogic
+    {
+        IGitUserRepository gitUserRepository;
+
+        public GitUserLogic(IGitUserRepository gitUserRepository)
+        {
+            this.gitUserRepository = gitUserRepository;
+        }
+        public int UserCount()
+        {
+            return gitUserRepository.ReadAll().Count();
+            //throw new NotImplementedException();
+        }
+    }
+}
