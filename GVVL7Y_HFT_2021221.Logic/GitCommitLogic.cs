22492eedc5256322c72766ff493e22df598b3f77
veladam01 <veladam01@gmail.com>
2021-11-16 09:30:50 +0100

    GitCommitLogic class created, implements IGitCommitLogic interface

---
+++ GVVL7Y_HFT_2021221.Logic/GitCommitLogic.cs
@@ -0,0 +1,31 @@
+﻿using GVVL7Y_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Logic
+{
+    class GitCommitLogic : IGitCommitLogic
+    {
+        IGitCommitRepository gitCommitRepository;
+
+        public GitCommitLogic(IGitCommitRepository gitCommitRepository)
+        {
+            this.gitCommitRepository = gitCommitRepository;
+        }
+
+        public int CommitCount()
+        {
+            return gitCommitRepository.ReadAll().Count(); 
+            //throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> CommitCountByUsers()
+        {
+            return gitCommitRepository.ReadAll().GroupBy(x => x.User).Select(x => new KeyValuePair<string, int>(x.Key.Name, x.Count()));
+            //throw new NotImplementedException();
+        }
+    }
+}
