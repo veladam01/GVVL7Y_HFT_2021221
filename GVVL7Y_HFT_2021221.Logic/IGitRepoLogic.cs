e14c01aec4c28732f919a271da5a0fd059e12dd9
veladam01 <veladam01@gmail.com>
2021-11-16 09:55:17 +0100

    IGitRepoLogic interface created

---
+++ GVVL7Y_HFT_2021221.Logic/IGitRepoLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Logic
+{
+    interface IGitRepoLogic
+    {
+        int RepoCount();
+
+        IEnumerable<KeyValuePair<string, int>> RepoCountByOwners();
+    }
+}
