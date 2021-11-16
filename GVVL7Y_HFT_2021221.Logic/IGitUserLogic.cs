3115e19359c04c44258a0f8dafc95550e41d90d5
veladam01 <veladam01@gmail.com>
2021-11-16 09:59:50 +0100

    IGitUserLogic interface created

---
+++ GVVL7Y_HFT_2021221.Logic/IGitUserLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Logic
+{
+    interface IGitUserLogic
+    {
+        int UserCount();
+
+        //no idea for other methods
+    }
+}
