e5dc8d37a923e68eb946fc7f90e326a806ffe6d3
veladam01 <veladam01@gmail.com>
2021-11-16 16:45:52 +0100

    NUnit Framework installed on Test layer

---
+++ GVVL7Y_HFT_2021221.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GVVL7Y_HFT_2021221.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
