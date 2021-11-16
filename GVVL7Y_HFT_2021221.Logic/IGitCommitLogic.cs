73b3d8bdf447502b8250b571f5f738a7a50bda6d
veladam01 <veladam01@gmail.com>
2021-11-16 09:09:24 +0100

    IGitCommitLogic interface created

---
+++ GVVL7Y_HFT_2021221.Logic/IGitCommitLogic.cs
@@ -0,0 +1,18 @@
+﻿using GVVL7Y_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVVL7Y_HFT_2021221.Logic
+{
+    interface IGitCommitLogic
+    {
+        
+
+        int CommitCount();
+
+        IEnumerable<KeyValuePair<string, int>> CommitCountByUsers();
+    }
+}
