eb8f528a4e18e4e1a3e1ef4b2f9640d9f2586c2d
veladam01 <veladam01@gmail.com>
2021-11-27 16:26:35 +0100

    Frontend Client created and tested

--- GVVL7Y_HFT_2021221.Client/Program.cs
+++ GVVL7Y_HFT_2021221.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GVVL7Y_HFT_2021221.Models;
+using System;
+using System.Linq;
 
 namespace GVVL7Y_HFT_2021221.Client
 {
@@ -6,7 +8,17 @@ namespace GVVL7Y_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(5000);
+
+            RestService rs = new RestService("http://localhost:39621/");
+
+            var res1 = rs.Get<GitCommit>("/gitcommit");
+
+            var res2 = rs.Get<GitRepo>("/gitrepo");
+
+            var res3 = rs.Get<GitUser>("/gituser");
+            ;
+            //Console.WriteLine("Hello World!");
         }
     }
 }
